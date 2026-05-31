8fd662e5c3f34e95a74f1f7108fd825692efa792
Daria Kondratyeva <kondratyevad@avride.ai>
2026-05-31 22:20:53 +0300

    02-javascript-data-types: sort strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,3 +1,10 @@
+const locales = ['ru', 'en'];
+
+const compareOptions = {
+  sensitivity: 'variant',
+  caseFirst: 'upper',
+};
+
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
@@ -5,5 +12,19 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  if (param !== 'asc' && param !== 'desc') {
+    console.log('Incorrect sorting type parameter. Please use "asc" or "desc"');
+    return [];
+  }
 
+  const arraySorted = [...arr];
+
+  if (param === 'asc') {
+    arraySorted.sort((a, b) => a.localeCompare(b, locales, compareOptions));
+  }
+  else if (param === 'desc') {
+    arraySorted.sort((a, b) => b.localeCompare(a, locales, compareOptions));
+  }
+
+  return arraySorted;
 }
