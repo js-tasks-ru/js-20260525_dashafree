e7b344ea339767e3bbf5a87fed1c7c3538cd8823
Daria Kondratyeva <kondratyevad@avride.ai>
2026-06-08 21:44:41 +0300

    03: invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -1,8 +1,12 @@
 /**
  * invertObj - should swap object keys and values
  * @param {object} obj - the initial object
- * @returns {object | undefined} - returns the new object or undefined if nothing did't pass
+ * @returns {object | undefined} - returns the new object or undefined if nothing didn't pass
  */
 export function invertObj(obj) {
-
+  if (!obj) {
+    return undefined;
+  }
+  const revertedArray = Object.entries(obj).map(([key, value]) => [value, key]);
+  return Object.fromEntries(revertedArray);
 }
