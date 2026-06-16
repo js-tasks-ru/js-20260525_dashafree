2165c5d1d50b6db67ffc0298937ab26bf1398481
Daria Kondratyeva <kondratyevad@avride.ai>
2026-06-16 21:49:02 +0300

    03: trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  }
 
+  if (size <= 0) {
+    return '';
+  }
+
+  let resultString = '';
+  let prevSymbol = '';
+  let counter = 0;
+
+  for (const symbol of string) {
+    if (symbol === prevSymbol) {
+      counter += 1;
+    }
+    else {
+      prevSymbol = symbol;
+      counter = 1;
+    }
+    if (counter <= size) {
+      resultString += symbol;
+    }
+  }
+  return resultString;
 }
